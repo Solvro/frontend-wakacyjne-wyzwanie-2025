52ad12ec9ba89deb4e276f6ccd285576550d65ab
Grzegorz Diaconescu <grzes.business@gmail.com>
2025-08-06 16:02:34 +0200

    Wykonane zadania 1 listy

--- 1. react-fundamentals/src/exercise/06.js
+++ 1. react-fundamentals/src/exercise/06.js
@@ -19,11 +19,37 @@ function UsernameForm({onSubmitUsername}) {
 
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
+  const inputRef = React.useRef()
+
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    // Get value from the ref instead of from event.target.elements
+    const value = inputRef.current.value
+
+    onSubmitUsername(value)
+
+    // Clear the input after submission
+    inputRef.current.value = ''
+  }
+
+  const [username, setUsername] = React.useState(null)
+
+  function handleChange(event) {
+    setUsername(event.target.value.toLowerCase())
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="input">Username:</label>
+        <input
+          id="input"
+          type="text"
+          value={username}
+          ref={inputRef}
+          onChange={handleChange}
+        />
       </div>
       <button type="submit">Submit</button>
     </form>
