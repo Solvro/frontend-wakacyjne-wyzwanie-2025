a8bbfbc535f363c1f0042722c94890ec3001e3ad
Zofia Walkowiak <walkowiak_zosia@wp.pl>
2025-08-10 22:49:07 +0200

    1. Zofia Walkowiak

--- 1. react-fundamentals/src/exercise/05.js
+++ 1. react-fundamentals/src/exercise/05.js
@@ -14,18 +14,32 @@ import '../box-styles.css'
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
 
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+function Box({style, size, ...otherProps}) {
+  const sizeClassName = size ? `box--${size}` : ''
+  return (
+    <div
+      className={`box ${sizeClassName}`}
+      style={{fontStyle: 'italic', ...style}}
+      {...otherProps}
+    />
+  )
+}
 
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box size="small" style={{backgroundColor: 'lightblue'}}>
+        small lightblue box
+      </Box>
+      <Box size="medium" style={{backgroundColor:'pink'}}>
+        medium pink box
+      </Box>
+      <Box size="large" style={{backgroundColor:'orange'}}>
+        large orange box
+      </Box>
     </div>
   )
 }
 
 export default App
+
