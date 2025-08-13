9c5f95312787569839f209c446fc40f14a771976
Grzegorz Diaconescu <grzes.business@gmail.com>
2025-08-13 11:26:12 +0200

    Wykonane zadania 2 listy

--- 2. react-hooks/src/exercise/01.js
+++ 2. react-hooks/src/exercise/01.js
@@ -3,12 +3,13 @@
 
 import * as React from 'react'
 
-function Greeting() {
+function Greeting({initialName}) {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  const [name, setName] = React.useState(initialName)
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
   }
 
   return (
@@ -23,7 +24,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Kate" />
 }
 
 export default App
