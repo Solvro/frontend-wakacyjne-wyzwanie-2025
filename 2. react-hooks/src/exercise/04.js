63d3f6227090765a85c1346ff53b8750578faed8
Dawid Błaszczyk <dawidbv1@gmail.com>
2025-08-15 21:55:42 +0200

    feat: update exercises with my solutions

--- 2. react-hooks/src/exercise/04.js
+++ 2. react-hooks/src/exercise/04.js
@@ -1,46 +1,143 @@
-// useState: tic tac toe
-// http://localhost:3000/isolated/exercise/04.js
+// Zadanie 0,1,2
+// import * as React from 'react'
+// import {useLocalStorageState} from '../utils'
 
+// // function useSetSquaresLocalHost(initialValue) {
+// //   const [squares, setSquares] = React.useState(initialValue)
+
+// //   React.useEffect(() => {
+// //     const storedSquares = localStorage.getItem('squares')
+// //     if (storedSquares) {
+// //       setSquares(JSON.parse(storedSquares))
+// //     }
+// //   }, [])
+
+// //   React.useEffect(() => {
+// //     localStorage.setItem('squares', JSON.stringify(squares))
+// //   }, [squares])
+
+// //   return [squares, setSquares]
+// // }
+
+// function Board() {
+//   const [squares, setSquares] = useLocalStorageState(
+//     'squares',
+//     Array(9).fill(null),
+//   )
+
+//   function selectSquare(square) {
+//     if (calculateWinner(squares) || squares[square]) {
+//       return
+//     }
+
+//     const nextValue = calculateNextValue(squares)
+//     const squaresCopy = [...squares]
+//     squaresCopy[square] = nextValue
+//     setSquares(squaresCopy)
+//   }
+
+//   function restart() {
+//     setSquares(Array(9).fill(null))
+//   }
+
+//   function renderSquare(i) {
+//     return (
+//       <button className="square" onClick={() => selectSquare(i)}>
+//         {squares[i]}
+//       </button>
+//     )
+//   }
+
+//   return (
+//     <div>
+//       <div className="status">
+//         {calculateStatus(
+//           calculateWinner(squares),
+//           squares,
+//           calculateNextValue(squares),
+//         )}
+//       </div>
+//       <div className="board-row">
+//         {renderSquare(0)}
+//         {renderSquare(1)}
+//         {renderSquare(2)}
+//       </div>
+//       <div className="board-row">
+//         {renderSquare(3)}
+//         {renderSquare(4)}
+//         {renderSquare(5)}
+//       </div>
+//       <div className="board-row">
+//         {renderSquare(6)}
+//         {renderSquare(7)}
+//         {renderSquare(8)}
+//       </div>
+//       <button className="restart" onClick={restart}>
+//         restart
+//       </button>
+//     </div>
+//   )
+// }
+
+// function Game() {
+//   return (
+//     <div className="game">
+//       <div className="game-board">
+//         <Board />
+//       </div>
+//     </div>
+//   )
+// }
+
+// // eslint-disable-next-line no-unused-vars
+// function calculateStatus(winner, squares, nextValue) {
+//   return winner
+//     ? `Winner: ${winner}`
+//     : squares.every(Boolean)
+//     ? `Scratch: Cat's game`
+//     : `Next player: ${nextValue}`
+// }
+
+// // eslint-disable-next-line no-unused-vars
+// function calculateNextValue(squares) {
+//   return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O'
+// }
+
+// // eslint-disable-next-line no-unused-vars
+// function calculateWinner(squares) {
+//   const lines = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6],
+//   ]
+//   for (let i = 0; i < lines.length; i++) {
+//     const [a, b, c] = lines[i]
+//     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+//       return squares[a]
+//     }
+//   }
+//   return null
+// }
+
+// function App() {
+//   return <Game />
+// }
+
+// export default App
+
+// Zadanie 3
 import * as React from 'react'
+import {useLocalStorageState} from '../utils'
 
-function Board() {
-  // 🐨 squares is the state for this component. Add useState for squares
-  const squares = Array(9).fill(null)
-
-  // 🐨 We'll need the following bits of derived state:
-  // - nextValue ('X' or 'O')
-  // - winner ('X', 'O', or null)
-  // - status (`Winner: ${winner}`, `Scratch: Cat's game`, or `Next player: ${nextValue}`)
-  // 💰 I've written the calculations for you! So you can use my utilities
-  // below to create these variables
-
-  // This is the function your square click handler will call. `square` should
-  // be an index. So if they click the center square, this will be `4`.
-  function selectSquare(square) {
-    // 🐨 first, if there's already a winner or there's already a value at the
-    // given square index (like someone clicked a square that's already been
-    // clicked), then return early so we don't make any state changes
-    //
-    // 🦉 It's typically a bad idea to mutate or directly change state in React.
-    // Doing so can lead to subtle bugs that can easily slip into production.
-    //
-    // 🐨 make a copy of the squares array
-    // 💰 `[...squares]` will do it!)
-    //
-    // 🐨 set the value of the square that was selected
-    // 💰 `squaresCopy[square] = nextValue`
-    //
-    // 🐨 set the squares to your copy
-  }
-
-  function restart() {
-    // 🐨 reset the squares
-    // 💰 `Array(9).fill(null)` will do it!
-  }
-
+function Board({onClick, squares}) {
   function renderSquare(i) {
     return (
-      <button className="square" onClick={() => selectSquare(i)}>
+      <button className="square" onClick={() => onClick(i)}>
         {squares[i]}
       </button>
     )
@@ -48,8 +145,6 @@ function Board() {
 
   return (
     <div>
-      {/* 🐨 put the status in the div below */}
-      <div className="status">STATUS</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -65,18 +160,82 @@ function Board() {
         {renderSquare(7)}
         {renderSquare(8)}
       </div>
-      <button className="restart" onClick={restart}>
-        restart
-      </button>
     </div>
   )
 }
 
 function Game() {
+  const [movesHistory, setMovesHistory] = React.useState([Array(9).fill(null)])
+  const [currentSquares, setSquares] = useLocalStorageState(
+    'currentSquares',
+    Array(9).fill(null),
+  )
+  const [historyIndex, setHistoryIndex] = React.useState(0)
+
+  function addMove(squaresCopy) {
+    const nextHistory = movesHistory.slice(0, historyIndex + 1)
+    setSquares(squaresCopy)
+    setMovesHistory(prevMoves => [
+      ...prevMoves.slice(0, historyIndex + 1),
+      squaresCopy,
+    ])
+    setHistoryIndex(nextHistory.length)
+  }
+
+  function backToMove(index) {
+    setSquares(movesHistory[index])
+    setHistoryIndex(index)
+  }
+
+  function selectSquare(square) {
+    if (calculateWinner(currentSquares) || currentSquares[square]) {
+      return
+    }
+
+    const nextValue = calculateNextValue(currentSquares)
+    const squaresCopy = [...currentSquares]
+    squaresCopy[square] = nextValue
+    addMove(squaresCopy)
+  }
+
+  function restart() {
+    setSquares(Array(9).fill(null))
+    setMovesHistory([])
+  }
+  const status = calculateStatus(
+    calculateWinner(currentSquares),
+    currentSquares,
+    calculateNextValue(currentSquares),
+  )
+
+  let moves = movesHistory.map((squares, index) => {
+    const description =
+      index === 0 ? 'Go to game start' : `Go to move #${index}`
+    const isCurrentMove = index === historyIndex
+    return (
+      <li key={index}>
+        <button onClick={() => backToMove(index)} disabled={isCurrentMove}>
+          {description} {isCurrentMove ? '(current)' : null}
+        </button>
+      </li>
+    )
+  })
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board
+          onClick={selectSquare}
+          squares={currentSquares}
+          status={status}
+        />
+        <button className="restart" onClick={restart}>
+          restart
+        </button>
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
       </div>
     </div>
   )
--- 2. react-hooks/src/exercise/06.js
+++ 2. react-hooks/src/exercise/06.js
@@ -1,32 +1,211 @@
-// useEffect: HTTP requests
-// http://localhost:3000/isolated/exercise/06.js
+// Zadanie 0, 1, 2
+// import * as React from 'react'
+// import {
+//   PokemonForm,
+//   fetchPokemon,
+//   PokemonDataView,
+//   PokemonInfoFallback,
+// } from '../pokemon'
 
+// function PokemonInfo({pokemonName}) {
+//   const [pokemon, setPokemon] = React.useState(null)
+//   const [error, setError] = React.useState(null)
+//   const [status, setStatus] = React.useState('idle')
+
+//   React.useEffect(() => {
+//     if (!pokemonName) return
+
+//     setPokemon(null)
+//     setError(null)
+//     setStatus('pending')
+//     fetchPokemon(pokemonName)
+//       .then(pokemon => {
+//         setPokemon(pokemon)
+//         setStatus('resolved')
+//       })
+//       .catch(error => {
+//         setError(error)
+//         setStatus('rejected')
+//       })
+//   }, [pokemonName])
+
+//   if (status === 'idle') {
+//     return 'Submit a pokemon'
+//   } else if (status === 'rejected') {
+//     return (
+//       <div role="alert">
+//         There was an error:{' '}
+//         <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+//       </div>
+//     )
+//   } else if (status === 'pending') {
+//     return <PokemonInfoFallback name={pokemonName} />
+//   } else if (status === 'resolved') {
+//     return <PokemonDataView pokemon={pokemon} />
+//   }
+// }
+
+// function App() {
+//   const [pokemonName, setPokemonName] = React.useState('')
+
+//   function handleSubmit(newPokemonName) {
+//     setPokemonName(newPokemonName)
+//   }
+
+//   return (
+//     <div className="pokemon-info-app">
+//       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
+//       <hr />
+//       <div className="pokemon-info">
+//         <PokemonInfo pokemonName={pokemonName} />
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// Zadanie 3, 4, 5
+// import * as React from 'react'
+// import {
+//   PokemonForm,
+//   fetchPokemon,
+//   PokemonDataView,
+//   PokemonInfoFallback,
+// } from '../pokemon'
+
+// class ErrorBoundary extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {error: null}
+//   }
+
+//   static getDerivedStateFromError(error) {
+//     return {error}
+//   }
+
+//   render() {
+//     if (this.state.error) {
+//       return (
+//         <div role="alert">
+//           There was an error:{' '}
+//           <pre style={{whiteSpace: 'normal'}}>{this.state.error.message}</pre>
+//         </div>
+//       )
+//     }
+
+//     return this.props.children
+//   }
+// }
+
+// function PokemonInfo({pokemonName}) {
+//   const [state, setState] = React.useState({
+//     pokemon: null,
+//     status: 'idle',
+//     error: null,
+//   })
+
+//   React.useEffect(() => {
+//     if (!pokemonName) return
+
+//     setState({pokemon: null, status: 'pending', error: null})
+//     fetchPokemon(pokemonName)
+//       .then(pokemon => {
+//         setState({pokemon, status: 'resolved', error: null})
+//       })
+//       .catch(error => {
+//         setState({pokemon: null, status: 'rejected', error})
+//       })
+//   }, [pokemonName])
+
+//   if (state.status === 'idle') {
+//     return 'Submit a pokemon'
+//   } else if (state.status === 'rejected') {
+//     throw state.error
+//     // return (
+//     //   <div role="alert">
+//     //     There was an error:{' '}
+//     //     <pre style={{whiteSpace: 'normal'}}>{state.error.message}</pre>
+//     //   </div>
+//     // )
+//   } else if (state.status === 'pending') {
+//     return <PokemonInfoFallback name={pokemonName} />
+//   } else if (state.status === 'resolved') {
+//     return <PokemonDataView pokemon={state.pokemon} />
+//   }
+// }
+
+// function App() {
+//   const [pokemonName, setPokemonName] = React.useState('')
+
+//   function handleSubmit(newPokemonName) {
+//     setPokemonName(newPokemonName)
+//   }
+
+//   return (
+//     <div className="pokemon-info-app">
+//       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
+//       <hr />
+//       <div className="pokemon-info">
+//         <ErrorBoundary key={pokemonName}>
+//           <PokemonInfo pokemonName={pokemonName} />
+//         </ErrorBoundary>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// Zadanie 6, 7, 8
 import * as React from 'react'
-// 🐨 you'll want the following additional things from '../pokemon':
-// fetchPokemon: the function we call to get the pokemon info
-// PokemonInfoFallback: the thing we show while we're loading the pokemon info
-// PokemonDataView: the stuff we use to display the pokemon info
-import {PokemonForm} from '../pokemon'
+import {
+  PokemonForm,
+  fetchPokemon,
+  PokemonDataView,
+  PokemonInfoFallback,
+} from '../pokemon'
+import {ErrorBoundary} from 'react-error-boundary'
 
 function PokemonInfo({pokemonName}) {
-  // 🐨 Have state for the pokemon (null)
-  // 🐨 use React.useEffect where the callback should be called whenever the
-  // pokemon name changes.
-  // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
-  // 💰 if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
-  // 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null.
-  // (This is to enable the loading state when switching between different pokemon.)
-  // 💰 Use the `fetchPokemon` function to fetch a pokemon by its name:
-  //   fetchPokemon('Pikachu').then(
-  //     pokemonData => {/* update all the state here */},
-  //   )
-  // 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
-  //   1. no pokemonName: 'Submit a pokemon'
-  //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
-  //   3. pokemon: <PokemonDataView pokemon={pokemon} />
+  const [state, setState] = React.useState({
+    pokemon: null,
+    status: 'idle',
+    error: null,
+  })
 
-  // 💣 remove this
-  return 'TODO'
+  React.useEffect(() => {
+    if (!pokemonName) return
+
+    setState({pokemon: null, status: 'pending', error: null})
+    fetchPokemon(pokemonName)
+      .then(pokemon => {
+        setState({pokemon, status: 'resolved', error: null})
+      })
+      .catch(error => {
+        setState({pokemon: null, status: 'rejected', error})
+      })
+  }, [pokemonName])
+
+  if (state.status === 'idle') {
+    return 'Submit a pokemon'
+  } else if (state.status === 'rejected') {
+    throw state.error
+  } else if (state.status === 'pending') {
+    return <PokemonInfoFallback name={pokemonName} />
+  } else if (state.status === 'resolved') {
+    return <PokemonDataView pokemon={state.pokemon} />
+  }
+}
+
+function ErrorFallback({error, resetErrorBoundary}) {
+  return (
+    <div role="alert">
+      There was an error:{' '}
+      <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  )
 }
 
 function App() {
@@ -36,12 +215,22 @@ function App() {
     setPokemonName(newPokemonName)
   }
 
+  function handleReset() {
+    setPokemonName('')
+  }
+
   return (
     <div className="pokemon-info-app">
       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
       <hr />
       <div className="pokemon-info">
-        <PokemonInfo pokemonName={pokemonName} />
+        <ErrorBoundary
+          FallbackComponent={ErrorFallback}
+          onReset={handleReset}
+          resetKeys={[pokemonName]}
+        >
+          <PokemonInfo pokemonName={pokemonName} />
+        </ErrorBoundary>
       </div>
     </div>
   )
