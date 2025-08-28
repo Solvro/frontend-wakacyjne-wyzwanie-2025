792340f85b2c22be611ee4824bb0044384d4f934
moshenetsb <bogdan.moshenets@outlook.com>
2025-08-28 21:04:57 +0200

    feat: zadanie 4

--- 4. advanced-react-patterns/src/exercise/05.extra-2.js
+++ 4. advanced-react-patterns/src/exercise/05.extra-2.js
@@ -1,6 +1,3 @@
-// State Reducer
-// http://localhost:3000/isolated/exercise/05.js
-
 import * as React from 'react'
 import {Switch} from '../switch'
 
@@ -9,12 +6,14 @@ const callAll =
   (...args) =>
     fns.forEach(fn => fn?.(...args))
 
+const actionTypes = {toggle: 'toggle', reset: 'reset'}
+
 function toggleReducer(state, {type, initialState}) {
   switch (type) {
-    case 'toggle': {
+    case actionTypes.toggle: {
       return {on: !state.on}
     }
-    case 'reset': {
+    case actionTypes.reset: {
       return initialState
     }
     default: {
@@ -29,8 +28,8 @@ function useToggle({initialOn = false, reducer = toggleReducer} = {}) {
   const [state, dispatch] = React.useReducer(reducer, initialState)
   const {on} = state
 
-  const toggle = () => dispatch({type: 'toggle'})
-  const reset = () => dispatch({type: 'reset', initialState})
+  const toggle = () => dispatch({type: actionTypes.toggle})
+  const reset = () => dispatch({type: actionTypes.reset, initialState})
 
   function getTogglerProps({onClick, ...props} = {}) {
     return {
@@ -56,25 +55,19 @@ function useToggle({initialOn = false, reducer = toggleReducer} = {}) {
   }
 }
 
+// export {useToggle, toggleReducer, actionTypes}
+
+// import {useToggle, toggleReducer, actionTypes} from './use-toggle'
+
 function App() {
   const [timesClicked, setTimesClicked] = React.useState(0)
   const clickedTooMuch = timesClicked >= 4
 
   function toggleStateReducer(state, action) {
-    switch (action.type) {
-      case 'toggle': {
-        if (clickedTooMuch) {
-          return {on: state.on}
-        }
-        return {on: !state.on}
-      }
-      case 'reset': {
-        return {on: false}
-      }
-      default: {
-        throw new Error(`Unsupported type: ${action.type}`)
-      }
+    if (action.type === actionTypes.toggle && clickedTooMuch) {
+      return {on: state.on}
     }
+    return toggleReducer(state, action)
   }
 
   const {on, getTogglerProps, getResetterProps} = useToggle({
