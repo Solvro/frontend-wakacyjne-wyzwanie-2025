27aa107b9179426d18d3113a0f215e9baef02b4f
Wiel0ch <kubawieloch11@gmail.com>
2025-08-29 17:53:04 +0200

    Rozwiazanie

--- 4. advanced-react-patterns/src/exercise/04.js
+++ 4. advanced-react-patterns/src/exercise/04.js
@@ -7,11 +7,8 @@ import {Switch} from '../switch'
 function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
-
-  // 🐨 Add a property called `togglerProps`. It should be an object that has
-  // `aria-pressed` and `onClick` properties.
-  // 💰 {'aria-pressed': on, onClick: toggle}
-  return {on, toggle}
+  const togglerProps = {'aria-pressed': on, onClick: toggle}
+  return {on, toggle,togglerProps}
 }
 
 function App() {
--- 4. advanced-react-patterns/src/exercise/05.js
+++ 4. advanced-react-patterns/src/exercise/05.js
@@ -21,12 +21,9 @@ function toggleReducer(state, {type, initialState}) {
 }
 
 // 🐨 add a new option called `reducer` that defaults to `toggleReducer`
-function useToggle({initialOn = false} = {}) {
+function useToggle({initialOn = false, reducer= toggleReducer} = {}) {
   const {current: initialState} = React.useRef({on: initialOn})
-  // 🐨 instead of passing `toggleReducer` here, pass the `reducer` that's
-  // provided as an option
-  // ... and that's it! Don't forget to check the 💯 extra credit!
-  const [state, dispatch] = React.useReducer(toggleReducer, initialState)
+  const [state, dispatch] = React.useReducer(reducer, initialState)
   const {on} = state
 
   const toggle = () => dispatch({type: 'toggle'})
@@ -61,20 +58,12 @@ function App() {
   const clickedTooMuch = timesClicked >= 4
 
   function toggleStateReducer(state, action) {
-    switch (action.type) {
-      case 'toggle': {
-        if (clickedTooMuch) {
-          return {on: state.on}
-        }
-        return {on: !state.on}
-      }
-      case 'reset': {
-        return {on: false}
-      }
-      default: {
-        throw new Error(`Unsupported type: ${action.type}`)
-      }
+    if (action.type === 'toggle' && clickedTooMuch) {
+      // Jeśli warunek jest spełniony, blokujemy zmianę stanu, zwracając go bez modyfikacji.
+      return {on: state.on}
     }
+    // W każdym innym przypadku, pozwalamy domyślnemu reducerowi zrobić swoje.
+    return toggleReducer(state, action)
   }
 
   const {on, getTogglerProps, getResetterProps} = useToggle({
