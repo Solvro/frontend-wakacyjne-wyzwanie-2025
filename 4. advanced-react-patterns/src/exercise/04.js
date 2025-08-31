70be41e8a4ce3ea2ca1827cb1785d4b27b0b3ca9
Matylda Strachota-Gorzycki <strachota.matylda@gmail.com>
2025-08-31 11:00:54 +0200

    feat: lista 4

--- 4. advanced-react-patterns/src/exercise/04.js
+++ 4. advanced-react-patterns/src/exercise/04.js
@@ -1,6 +1,3 @@
-// Prop Collections and Getters
-// http://localhost:3000/isolated/exercise/04.js
-
 import * as React from 'react'
 import {Switch} from '../switch'
 
@@ -8,10 +5,14 @@ function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  // 🐨 Add a property called `togglerProps`. It should be an object that has
-  // `aria-pressed` and `onClick` properties.
-  // 💰 {'aria-pressed': on, onClick: toggle}
-  return {on, toggle}
+  return {
+    on,
+    toggle,
+    togglerProps: {
+      'aria-pressed': on,
+      onClick: toggle,
+    },
+  }
 }
 
 function App() {
@@ -28,8 +29,3 @@ function App() {
 }
 
 export default App
-
-/*
-eslint
-  no-unused-vars: "off",
-*/
