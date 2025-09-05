acc64d2645997165f9b49992a051369e9e47a73e
patmik <patmikdev@gmail.com>
2025-09-05 17:44:58 +0200

    feat: part 5 solution

--- 5. react-performance/src/exercise/01.js
+++ 5. react-performance/src/exercise/01.js
@@ -2,19 +2,24 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-// 💣 remove this import
-import Globe from '../globe'
 
 // 🐨 use React.lazy to create a Globe component which uses a dynamic import
 // to get the Globe component from the '../globe' module.
 
+
 function App() {
   const [showGlobe, setShowGlobe] = React.useState(false)
+
+  const loadGlobe = () => import('../globe')
+  const Globe = React.lazy(loadGlobe)
+
 
   // 🐨 wrap the code below in a <React.Suspense /> component
   // with a fallback.
   // 💰 try putting it in a few different places and observe how that
   // impacts the user experience.
+
+
   return (
     <div
       style={{
@@ -26,17 +31,24 @@ function App() {
         padding: '2rem',
       }}
     >
-      <label style={{marginBottom: '1rem'}}>
+      <label style={{marginBottom: '1rem'}}
+        
+        onMouseEnter={loadGlobe}
+        onFocus={loadGlobe}
+      >
         <input
           type="checkbox"
           checked={showGlobe}
           onChange={e => setShowGlobe(e.target.checked)}
+          
         />
         {' show globe'}
       </label>
+      <React.Suspense fallback={<div>Loading globe...</div>}>
       <div style={{width: 400, height: 400}}>
         {showGlobe ? <Globe /> : null}
       </div>
+      </React.Suspense> 
     </div>
   )
 }
