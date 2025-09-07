8ed90e5653ac1f0b5116e95d39f3833223f6b06a
CichowskiK <femat0244@gmail.com>
2025-09-07 13:01:19 +0200

    lista 5

--- 5. react-performance/src/exercise/02.js
+++ 5. react-performance/src/exercise/02.js
@@ -60,8 +60,7 @@ function App() {
   const forceRerender = useForceRerender()
   const [inputValue, setInputValue] = React.useState('')
 
-  // 🐨 wrap getItems in a call to `React.useMemo`
-  const allItems = getItems(inputValue)
+  const allItems = React.useMemo(() =>getItems(inputValue))
   const items = allItems.slice(0, 100)
 
   const {
